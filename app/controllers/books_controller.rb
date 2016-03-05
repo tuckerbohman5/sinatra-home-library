a11ce40eca9e0f599c637c81c38ffa7d86220574
tuckerbohman5 <tucker.bohman@gmail.com>
2016-03-05 16:26:57 -0700

    built book controller functionality to build new books and after testing it is working

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,4 +9,28 @@ class BooksController < ApplicationController
     @authors = Author.all
     erb :'/books/new'
   end
+
+  post '/books' do 
+    
+    @book = Book.new
+    @author = Author.find(params[:author_id])
+
+    if @author 
+      @book.author = @author
+    else 
+      @author_new = Author.create(params[:author])
+      @book.author = @author_new
+    end
+    @book.title = params[:title]
+    
+    @book.save
+
+    redirect "/books/#{@book.id}"
+  end
+
+  get '/books/:id' do 
+    @book = Book.find(params[:id])
+    erb :'/books/show'
+  end
+
 end
--- app/models/author.rb
+++ app/models/author.rb
@@ -1,3 +1,8 @@
 class Author < ActiveRecord::Base
   has_many :books
+
+  def display_name
+    # Rowling, J.K
+    self.last_name + ", " + self.first_name
+  end
 end
