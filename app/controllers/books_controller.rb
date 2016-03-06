0ccbbd202867ad24fae4c9b378292e714ba81f7c
tuckerbohman5 <tucker.bohman@gmail.com>
2016-03-05 23:31:01 -0700

    added validation to edit book action

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -44,15 +44,24 @@ class BooksController < ApplicationController
   get '/books/:id/edit' do 
     redirect_if_not_logged_in
     @book = Book.find(params[:id])
+    @error_message = params[:error]
     erb :'/books/edit'
   end
 
   post '/books/:id' do 
     redirect_if_not_logged_in
     @book = Book.find(params[:id])
+    if params[:title] != ""
     @book.update(title: params[:title])
+    else
+      redirect "/books/#{@book.id}/edit?error=Please enter a valid title!"
+    end
+    if params[:author][:first_name] != "" && params[:author][:last_name] != ""
     @book.author.update(params[:author])
     redirect "/books/#{@book.id}"
+    else
+      redirect "/books/#{@book.id}/edit?error=Please enter a valid author!"
+    end
   end
 
   get '/books/:id/delete' do 
